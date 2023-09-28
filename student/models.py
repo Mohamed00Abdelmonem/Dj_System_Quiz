5378c39e153de7f60ad182df06f3b84c16d1630c
unknown <mmohamedabdelm@gmail.com>
2023-09-28 16:00:09 +0300

    add prject quiz p1

---
+++ student/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+YEAR_CHOICES = [
+        ('one', 'one'),
+        ('two', 'two'),
+        ('three', 'three'),
+    ]
+
+
+class Student(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    profile_pic= models.ImageField(upload_to='profile_pic/Student/',null=True,blank=True)
+    address = models.CharField(max_length=40)
+    mobile = models.CharField(max_length=20,null=False)
+    year_of_study = models.CharField(max_length=10, choices=YEAR_CHOICES, null=True, blank=True)
+ 
+    @property
+    def get_name(self):
+        return self.user.first_name+" "+self.user.last_name
+    @property
+    def get_instance(self):
+        return self
+    def __str__(self):
+        return self.user.first_name
